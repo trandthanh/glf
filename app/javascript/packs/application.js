6e237513de3c7907b556d46b1215d1f6eb60b5e0
diane johnston <diane.jroussillon@gmail.com>
2019-07-10 19:29:58 +0200

    changed the navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,14 @@
 import "bootstrap";
+import "jquery";
 
 import { sideNav } from '../components/sidenav'
+import { scrolledNav } from '../components/scrollednav'
 
 
 if (document.querySelector(".navbar-mobile")) {
   sideNav();
+}
+
+if (document.querySelector(".navbar")) {
+  scrolledNav();
 }
