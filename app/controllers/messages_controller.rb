a3b5b68fc55d8266782ec74ea722966e672e0b7f
diane johnston <diane.jroussillon@gmail.com>
2019-07-22 16:53:35 +0200

    changed the email addresses

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,9 @@
 class MessagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:create]
+  skip_before_action :authenticate_user!, only: [:create, :new]
+
+  def new
+    @message = Message.new(params[:message])
+  end
 
   def create
     @message = Message.new(params[:message])
@@ -9,10 +13,10 @@ class MessagesController < ApplicationController
         @message = Message.new
         @mail = params[:message][:recipient_email]
         @titre = params[:message][:titre]
-        format.html { render 'services/gestionlocative' }
+        format.html { render 'new' }
         format.js   { flash[:success] = @notice = "Merci pour votre message, nous revenons vers vous au plus vite !" }
       else
-        format.html { render 'services/gestionlocative' }
+        format.html { render 'new' }
         format.js   { flash[:error] = @notice = "Une erreur s'est produite, veuillez réessayer ou nous contacter au 01 44 18 18 18." }
       end
     end
